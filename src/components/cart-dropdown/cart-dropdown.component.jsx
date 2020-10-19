ea09eecfc72c5dcf4e657a2659d89d7bdf737c66
Daniel <Daniellagowskii@gmail.com>
2020-10-19 10:18:13 +0200

    updating our application  with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,6 +8,8 @@ import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
 
 import CartItem from '../cart-item/cart-item.component';
+
+import {selectCartItems} from '../../redux/cart/cart.selectors';
  
 
 
@@ -15,15 +17,17 @@ import CartItem from '../cart-item/cart-item.component';
 const CartDropdown = ({cartItems}) => (
    <div className="cart-dropdown">
       <div className="cart-items">
-         {cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)}
+         {
+             cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+         }
       </div>
       <CustomButton>GOT TO CHECKOUT</CustomButton>
    </div>
 )
 
 
-const mapStateToProps = ({cart:{cartItems}}) => ({
-   cartItems
+const mapStateToProps = state => ({
+   cartItems:selectCartItems(state)
 })
 
 
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 
 
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
 
 
 
@@ -11,7 +12,7 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden,cartItems}) => {
+const CartIcon = ({toggleCartHidden,itemCount}) => {
    
 
 
@@ -20,7 +21,7 @@ const CartIcon = ({toggleCartHidden,cartItems}) => {
    return(
    <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartItems.length}</span>
+      <span className="item-count">{itemCount}</span>
    </div>
 )}
 
@@ -29,9 +30,8 @@ const mapDispatchToProps = dispatch => ({
 });
 
 
-
-const mapStateToProps = ({cart:{cartItems}}) => ({
-   cartItems
+const mapStateToProps = state => ({
+   itemCount:selectCartItemsCount(state)
 }) 
 
 
